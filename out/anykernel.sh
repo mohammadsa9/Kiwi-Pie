5abcc615f2d5f933ceb2308cf54eae513e11901a
mohammadsa9 <mohammadsa78@yahoo.com>
2020-08-08 14:05:36 +0430

    Anykernel Config Corrected

--- out/anykernel.sh
+++ out/anykernel.sh
@@ -4,7 +4,7 @@
 ## AnyKernel setup
 # begin properties
 properties() { '
-kernel.string=~HotDoG Kernel by MohammadSa9~
+kernel.string=~ HotDoG Kernel by MohammadSa9 ~
 do.devicecheck=0
 do.modules=0
 do.systemless=0
@@ -20,7 +20,7 @@ supported.patchlevels=
 '; } # end properties
 
 # shell variables
-block=/dev/block/platform/omap/omap_hsmmc.0/by-name/boot;
+block=/dev/block/bootdevice/by-name/boot;
 is_slot_device=0;
 ramdisk_compression=auto;
 
